4e2d82ddbb66f5d90544bd46e249f48f6b9b23ca
Tasdeed Aziz <ayonaziz@gmail.com>
2022-11-28 15:31:53 -0500

    optimised category code

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Women",
+    },
+    {
+      id: 5,
+      title: "Men",
+    },
+  ];
+
   return (
     <div className="contrainer-wrapper">
       <div className="categories-container">
         <div className="category-container">
-          <div className="category-body-container">
-            <h2>Hats</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          <div className="category-body-container">
-            <h2>Jackets</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          <div className="category-body-container">
-            <h2>Sneakers</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          <div className="category-body-container">
-            <h2>Women</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          <div className="category-body-container">
-            <h2>Men</h2>
-            <p>Shop Now</p>
-          </div>
+          {categories.map((cat) => {
+            return (
+              <div key={cat.id} className="category-body-container">
+                <h2>{cat.title}</h2>
+                <p>Shop Now</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
