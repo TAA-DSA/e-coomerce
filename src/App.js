1a52c4a76177af9f3599a0cec73568869f44f5cb
Tasdeed Aziz <ayonaziz@gmail.com>
2022-11-28 15:11:05 -0500

    shop categories created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 function App() {
   return (
-    <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+    <div className="contrainer-wrapper">
+      <div className="categories-container">
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>Jackets</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>Sneakers</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>Women</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          <div className="category-body-container">
+            <h2>Men</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
       </div>
     </div>
