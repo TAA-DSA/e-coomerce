7bd7b864ae6d6ab44589dbbf13e00814325dc428
Tasdeed Aziz <ayonaziz@gmail.com>
2022-11-28 15:04:03 -0500

    category container created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
-
 function App() {
   return (
     <div className="categories-container">
       <div className="category-container">
-        <div className="category-body-container"></div>
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
       </div>
     </div>
   );
