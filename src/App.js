e965cd538cbe92eb9db30c8dc672f053edb2717a
Tasdeed Aziz <ayonaziz@gmail.com>
2022-11-28 23:59:56 -0500

    created category items component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import Categories from "./components/Data/data";
-import "./components/Styles/categories.styles.scss";
+import CategoryItems from "./components/category-item/catrgory-item.component";
 
 function App() {
   return (
     <div className="categories-container">
-      {Categories.map(({ title, id }) => (
-        <div key={id} className="category-container">
-          <div className="background-image" />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {Categories.map((category) => (
+        <CategoryItems key={category.id} category={category} />
       ))}
     </div>
   );
