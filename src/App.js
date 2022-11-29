19129630fe0b6e1295132a014568600a91061c3a
Tasdeed Aziz <ayonaziz@gmail.com>
2022-11-28 21:55:40 -0500

    added data component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Women",
-    },
-    {
-      id: 5,
-      title: "Men",
-    },
-  ];
+import Categories from "./components/Data/data";
 
+function App() {
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => (
+      {Categories.map(({ title, id }) => (
         <div key={id} className="category-container">
           <div className="background-image" />
           <div className="category-body-container">
