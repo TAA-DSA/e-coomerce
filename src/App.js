86fbae289a096e650687b023d9f1ebe83190a118
Tasdeed Aziz <ayonaziz@gmail.com>
2022-11-29 00:45:35 -0500

    font added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Category from "./components/category/category-component";
+import Categories from "./components/Data/data-category";
 
 function App() {
-  return <Category />;
+  return <Category directory={Categories} />;
 }
 
 export default App;
--- src/components/category/category-component.js
+++ src/components/category/category-component.js
@@ -1,11 +1,10 @@
-import Categories from "../Data/data-category";
 import CategoryItems from "../category-item/catrgory-item.component";
 import "../category/category-component-styles.scss";
 
-const category = () => {
+const category = ({ directory }) => {
   return (
     <div className="categories-container">
-      {Categories.map((category) => (
+      {directory.map((category) => (
         <CategoryItems key={category.id} category={category} />
       ))}
     </div>
