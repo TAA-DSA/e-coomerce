bd8508e0976bf4393e2292df03f20ea1571d5dad
Tasdeed Aziz <ayonaziz@gmail.com>
2022-12-01 15:50:53 -0500

    google in redirect signin implemented

--- src/components/utils/firebase/firebase.utils.js
+++ src/components/utils/firebase/firebase.utils.js
@@ -9,6 +9,7 @@ import {
 } from "firebase/auth";
 
 //Firestore Data imports
+
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -30,6 +31,8 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
 
