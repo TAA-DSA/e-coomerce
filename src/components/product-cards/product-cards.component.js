ff4ffd29569afcf21b6b51aafffef91d92ddacbc
Tasdeed Aziz <ayonaziz@gmail.com>
2022-12-16 02:10:05 -0500

    added functionality for cart drop-down box

--- src/components/product-cards/product-cards.component.js
+++ src/components/product-cards/product-cards.component.js
@@ -1,8 +1,14 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import "./product-card.styles.scss";
 import Button from "../button/button.component";
 
 const ProductCards = ({ product }) => {
   const { name, price, imageUrl } = product;
+
+  const { addItemToCart } = useContext(CartContext);
+  const addProductsToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -10,7 +16,9 @@ const ProductCards = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to cart</Button>
+      <Button buttonType="inverted" onClick={addProductsToCart}>
+        Add to cart
+      </Button>
     </div>
   );
 };
